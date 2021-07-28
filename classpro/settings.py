57dcd56d50bc852e1f5ba78a5d9742f8c67be5f3
pooja-pawar06 <pawar.pooja5264@gmail.com>
2021-07-28 14:37:55 +0530

    Project Updated

--- classpro/settings.py
+++ classpro/settings.py
@@ -21,6 +21,7 @@ django_heroku.settings(locals())
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env(SECRET_KEY=str,)
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 
 
 # Quick-start development settings - unsuitable for production
